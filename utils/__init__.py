453be3538c455e4ebcb1240a05e4439d9a3af08d
rabryan94-sys <ra.bryan94@gmail.com>
2026-01-19 00:14:56 +0100

    Initialize utils module with imports

---
+++ utils/__init__.py
@@ -0,0 +1,28 @@
+"""
+Baddie's Bakery - Module Utils
+"""
+
+from utils.prompts import (
+    BRAND_IDENTITY, 
+    PROMPTS, 
+    PLATFORM_GUIDELINES,
+    CITATION_THEMES,
+    EDUCATIONAL_TOPICS
+)
+
+from utils.generator import (
+    generate_post,
+    generate_launch_post,
+    generate_citation_post,
+    generate_educational_post
+)
+
+from utils.database import (
+    save_post,
+    get_all_posts,
+    get_posts_by_type,
+    get_posts_by_platform,
+    get_post_by_id,
+    delete_post,
+    get_stats
+)
