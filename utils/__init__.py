71ba38da520c7b10fd9d631a62a5fe4cd63cbbdb
rabryan94-sys <ra.bryan94@gmail.com>
2026-01-19 23:34:00 +0100

    Simplify import statements in utils/__init__.py
    Refactored import statements for clarity and organization.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,30 +1,20 @@
-"""
-Baddie's Bakery - Module Utils
-"""
+from utils.prompts import BRAND_IDENTITY
+from utils.prompts import PROMPTS
+from utils.prompts import PLATFORM_GUIDELINES
+from utils.prompts import CITATION_THEMES
+from utils.prompts import EDUCATIONAL_TOPICS
+from utils.prompts import VIDEO_TYPES
 
-from utils.prompts import (
-    BRAND_IDENTITY, 
-    PROMPTS, 
-    PLATFORM_GUIDELINES,
-    CITATION_THEMES,
-    EDUCATIONAL_TOPICS,
-    VIDEO_TYPES
-)
+from utils.generator import generate_post
+from utils.generator import generate_launch_post
+from utils.generator import generate_citation_post
+from utils.generator import generate_educational_post
+from utils.generator import generate_video_script
 
-from utils.generator import (
-    generate_post,
-    generate_launch_post,
-    generate_citation_post,
-    generate_educational_post,
-    generate_video_script
-)
-
-from utils.database import (
-    save_post,
-    get_all_posts,
-    get_posts_by_type,
-    get_posts_by_platform,
-    get_post_by_id,
-    delete_post,
-    get_stats
-)
+from utils.database import save_post
+from utils.database import get_all_posts
+from utils.database import get_posts_by_type
+from utils.database import get_posts_by_platform
+from utils.database import get_post_by_id
+from utils.database import delete_post
+from utils.database import get_stats
